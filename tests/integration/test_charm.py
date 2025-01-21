5bb8f06614eaa2dbc233650eef4729236123a0a9
artivis <jeremie.deray@canonical.com>
2025-01-21 14:35:27 +0100

    add loki integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,19 +2,18 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import asyncio
 import logging
 from pathlib import Path
 
 import pytest
 import yaml
-from charmed_kubeflow_chisme.testing import assert_logging
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 RESOURCE_NAME = "cos-registration-server-image"
+RESOURCE_PATH = METADATA["resources"][RESOURCE_NAME]["upstream-source"]
 APP_NAME = METADATA["name"]
 
 
@@ -26,18 +25,17 @@ async def test_build_and_deploy(ops_test: OpsTest):
     """
     # Build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
-    resources = {RESOURCE_NAME: METADATA["resources"][RESOURCE_NAME]["upstream-source"]}
+    resources = {RESOURCE_NAME: RESOURCE_PATH}
 
-    # Deploy the charm and wait for active/idle status
-    await asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
-        ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
-        ),
+    # Deploy the charm
+    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME)
+
+    # and wait for active/idle status
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
     )
 
 
-async def test_logging(self, ops_test: OpsTest):
-    """Test logging is defined in relation data bag."""
-    app = ops_test.model.applications[APP_NAME]
-    await assert_logging(app)
+async def test_status(ops_test):
+    """Assert on the unit status."""
+    assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
